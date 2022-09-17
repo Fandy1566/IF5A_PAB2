5ec62c0665ac0bdc3f07bea62e14537249c7779c
Fandy <100325135+Fandy1566@users.noreply.github.com>
2022-09-17 12:48:53 +0700

    Pertemuan 2

--- Pertemuan 1 - Notifikasiku/app/src/main/java/com/if5a/notifikasiku/MainActivity.java
+++ Pertemuan 1 - Notifikasiku/app/src/main/java/com/if5a/notifikasiku/MainActivity.java
@@ -7,18 +7,26 @@ import androidx.core.content.ContextCompat;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.view.View;
 
 import com.if5a.notifikasiku.databinding.ActivityMainBinding;
 
+import java.io.IOException;
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    private int notifId = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +34,10 @@ public class MainActivity extends AppCompatActivity {
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        //tambahkan StrictMode jika force close
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
 
         binding.btnNotify.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,10 +48,22 @@ public class MainActivity extends AppCompatActivity {
 
     }
         private void showNotification() {
+            PendingIntent pendingIntent;
+            Intent intent = new Intent(this,MainActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            int pendingIntentFlag = PendingIntent.FLAG_UPDATE_CURRENT;
+
+            pendingIntent = PendingIntent.getActivity(this,0, intent, pendingIntentFlag);
+
             NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this,"notifikasiku_default")
-                    .setContentTitle("Notifikasiku")
-                    .setContentText("Jangan lupa tugas PAB 2")
+                    .setContentTitle("Panggilan Terakhir Reminder Tugas")
+                    .setContentText("Panggilan Terakhir Reminder Tugas PAB2")
                     .setAutoCancel(true)
+                    .setContentIntent(pendingIntent)
+//                    .setStyle(new NotificationCompat.BigTextStyle()
+//                            .setBigContentTitle("Panggilan Terakhir Reminder Tugas")
+//                            .bigText("Panggilan Terakhir Reminder Tugas PAB2. Pastikan jawaban yang dikumpul sesuai dengan intruksi!")
+//                            .setSummaryText("Panggilan Terakhir Reminder Tugas"))
                     .setContentInfo("Notifikasiku")
                     .setLargeIcon(BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher))
                     .setColor(ContextCompat.getColor(this, android.R.color.transparent))
@@ -47,7 +71,28 @@ public class MainActivity extends AppCompatActivity {
                     .setDefaults(Notification.DEFAULT_VIBRATE)
                     .setNumber(3)
                     .setSmallIcon(R.drawable.ic_baseline_notifications_24);
+
+            String picture = "https://static.vecteezy.com/system/resources/thumbnails/004/581/274/small/bell-alert-notification-icon-set-free-vector.jpg";
+
+            try {
+                if (picture != null && !picture.equals("")) {
+                    URL url = new URL(picture);
+                    Bitmap bigPicture = BitmapFactory.decodeStream(url.openConnection().getInputStream());
+                    notificationBuilder.setStyle(new NotificationCompat.BigPictureStyle()
+                            .bigPicture(bigPicture)
+                            .setBigContentTitle("Panggilan Terakhir Reminder Tugas")
+                            .setSummaryText("Panggilan Terakhir Reminder Tugas PAB2"));
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
             NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                pendingIntentFlag = PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_MUTABLE;
+            }
+
             if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
                 NotificationChannel channel = new NotificationChannel("notifikasiku_default","notifikasiku", NotificationManager.IMPORTANCE_DEFAULT);
                 channel.setDescription("");
@@ -64,7 +109,7 @@ public class MainActivity extends AppCompatActivity {
             }
 
             if(notificationManager != null){
-                notificationManager.notify(0,notificationBuilder.build());
+                notificationManager.notify(notifId++,notificationBuilder.build());
             }
         }
 
