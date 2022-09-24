e80d33d7e0b3e5eb53b94afd9644494555cf870e
Fandy <100325135+Fandy1566@users.noreply.github.com>
2022-09-24 11:22:14 +0700

    update

--- Pertemuan 4 - Peta/app/src/main/java/com/if5a/peta/MainActivity.java
+++ Pertemuan 4 - Peta/app/src/main/java/com/if5a/peta/MainActivity.java
@@ -1,14 +1,51 @@
 package com.if5a.peta;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.if5a.peta.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+    private ActivityMainBinding binding;
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate((getLayoutInflater()));
+        setContentView(binding.getRoot());
+
+        SupportMapFragment mapFragment =  (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap){
+        mMap = googleMap;
+        mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        LatLng latLngUSer = new LatLng(-2.962752506769098, 104.74000029930386);
+        mMap.addMarker(new MarkerOptions()
+                .position(latLngUSer)
+                .title("Lokasi Saya"))
+                .showInfoWindow();
+        mMap.addCircle(new CircleOptions()
+                .center(latLngUSer)
+                .radius(100)
+                .strokeColor(Color.TRANSPARENT)
+                .fillColor(R.color.purple_500));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLngUSer,17));
     }
 }
