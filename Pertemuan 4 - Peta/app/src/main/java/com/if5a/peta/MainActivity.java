259b64c834c296cdd0256de7944bdf92cafd94f3
Rizky Kurniawan <100325135+Fandy1566@users.noreply.github.com>
2022-10-01 11:24:25 +0700

    update pert 6

--- Pertemuan 4 - Peta/app/src/main/java/com/if5a/peta/MainActivity.java
+++ Pertemuan 4 - Peta/app/src/main/java/com/if5a/peta/MainActivity.java
@@ -37,8 +37,8 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     private ActivityMainBinding binding;
     private GoogleMap mMap;
-    private List<lokasi> restaurantList = new ArrayList<>();
-    private List<lokasi> hospitalList = new ArrayList<>();
+    private List<Lokasi> restaurantList = new ArrayList<>();
+    private List<Lokasi> hospitalList = new ArrayList<>();
     private ActivityResultLauncher<String[]> locationPermissionRequest = registerForActivityResult(new ActivityResultContracts.RequestMultiplePermissions(), result -> {
         Boolean fineLocationGranted = result.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false);
         Boolean coarseLocationGranted = result.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false);
@@ -66,21 +66,21 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
 
 
-        restaurantList.add(new lokasi("Kinnicheesetea_kamboja", new LatLng(-2.9682594183643283, 104.74248872494825)));
-        restaurantList.add(new lokasi("Warung Dwi", new LatLng(-2.96774135843591, 104.74047234383686)));
-        restaurantList.add(new lokasi("PEMPEK DAN TEKWAN UDANG MIRNA", new LatLng(-2.9675419447358844, 104.74055879282491)));
-        restaurantList.add(new lokasi("R.M RANG TANJUNG Ariodillah", new LatLng(-2.9673470725734377, 104.74049481874668)));
-        restaurantList.add(new lokasi("Rumah Makan Surya", new LatLng(-2.96769713682167, 104.73984323246108)));
-        restaurantList.add(new lokasi("Ayam gepuk & lele kriuk", new LatLng(-2.9668048238885336, 104.73916921099863)));
-        restaurantList.add(new lokasi("Aditia Masakan Padang", new LatLng(-2.966097669561217, 104.74022063695662)));
-        restaurantList.add(new lokasi("Apis fried chicken", new LatLng(-2.9654504488438254, 104.74004536534841)));
-        restaurantList.add(new lokasi("Catering Qodri", new LatLng(-2.9665457402379416, 104.74135536830319)));
-        restaurantList.add(new lokasi("Saladmom trikora", new LatLng(-2.9678521614727775, 104.73993346090307)));
-        hospitalList.add(new lokasi("Rumah Sakit Umum Pusat Dr. Mohammad Hoesin Instalasi Gawat Darurat", new LatLng(-2.9672710816952863, 104.75069087570334)));
-        hospitalList.add(new lokasi("Rsmh", new LatLng(-2.965330480891752, 104.74594853799859)));
-        hospitalList.add(new lokasi("Rumah Sakit Mata Sriwijaya Eye Centre Palembang", new LatLng(-2.9705786497711784, 104.75074175735665)));
-        hospitalList.add(new lokasi("RSIA YK MADIRA", new LatLng(-2.9725225575818364, 104.752341002132)));
-        hospitalList.add(new lokasi("Rumah Sakit RK. Charitas", new LatLng(-2.9744861904008233, 104.7526238227884)));
+        restaurantList.add(new Lokasi("Kinnicheesetea_kamboja", new LatLng(-2.9682594183643283, 104.74248872494825)));
+        restaurantList.add(new Lokasi("Warung Dwi", new LatLng(-2.96774135843591, 104.74047234383686)));
+        restaurantList.add(new Lokasi("PEMPEK DAN TEKWAN UDANG MIRNA", new LatLng(-2.9675419447358844, 104.74055879282491)));
+        restaurantList.add(new Lokasi("R.M RANG TANJUNG Ariodillah", new LatLng(-2.9673470725734377, 104.74049481874668)));
+        restaurantList.add(new Lokasi("Rumah Makan Surya", new LatLng(-2.96769713682167, 104.73984323246108)));
+        restaurantList.add(new Lokasi("Ayam gepuk & lele kriuk", new LatLng(-2.9668048238885336, 104.73916921099863)));
+        restaurantList.add(new Lokasi("Aditia Masakan Padang", new LatLng(-2.966097669561217, 104.74022063695662)));
+        restaurantList.add(new Lokasi("Apis fried chicken", new LatLng(-2.9654504488438254, 104.74004536534841)));
+        restaurantList.add(new Lokasi("Catering Qodri", new LatLng(-2.9665457402379416, 104.74135536830319)));
+        restaurantList.add(new Lokasi("Saladmom trikora", new LatLng(-2.9678521614727775, 104.73993346090307)));
+        hospitalList.add(new Lokasi("Rumah Sakit Umum Pusat Dr. Mohammad Hoesin Instalasi Gawat Darurat", new LatLng(-2.9672710816952863, 104.75069087570334)));
+        hospitalList.add(new Lokasi("Rsmh", new LatLng(-2.965330480891752, 104.74594853799859)));
+        hospitalList.add(new Lokasi("Rumah Sakit Mata Sriwijaya Eye Centre Palembang", new LatLng(-2.9705786497711784, 104.75074175735665)));
+        hospitalList.add(new Lokasi("RSIA YK MADIRA", new LatLng(-2.9725225575818364, 104.752341002132)));
+        hospitalList.add(new Lokasi("Rumah Sakit RK. Charitas", new LatLng(-2.9744861904008233, 104.7526238227884)));
 
 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
@@ -123,7 +123,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             }
         });
 
-        binding.flReataurant.setOnClickListener(new View.OnClickListener() {
+        binding.fabRestaurant.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 mMap.clear();
@@ -138,7 +138,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
             }
         });
-        binding.flHospital.setOnClickListener(new View.OnClickListener() {
+        binding.fabHospital.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 mMap.clear();
